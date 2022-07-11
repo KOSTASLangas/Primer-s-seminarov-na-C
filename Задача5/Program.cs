4eb4d3f575bdc2f705b279c38d3d7b92c6e4277c
Kostas <langas.kostya@mail.ru>
2022-07-11 15:33:16 +0500

    нахождение последней цифры числа

---
+++ Задача5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напиши программу, которая принимает на вход трехзначное число
+// и на выходе показывает последнюю его цифру.
+
+Console.WriteLine ("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.WriteLine ("Неверное число");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine (n % 10);
