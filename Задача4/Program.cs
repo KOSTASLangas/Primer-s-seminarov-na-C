56de0f5f0a72e989bdac1117e9319e7882231e37
Kostas <langas.kostya@mail.ru>
2022-07-11 15:12:55 +0500

    целые числа от отриц до полож

---
+++ Задача4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиши программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке [-N,N].
+
+int n ;
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+
+int i = (-1) * n;
+
+while (i <= n )
+{
+    Console.WriteLine(i);
+    i++;
+
+}
