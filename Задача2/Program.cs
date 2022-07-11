e4ab7e26ea783404f59f16fdb920b241a265520d
Kostas <langas.kostya@mail.ru>
2022-07-11 14:35:48 +0500

    Сравнение числа и его квадрата

---
+++ Задача2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+int a,b;
+Console.WriteLine ("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine());
+b = b*b;
+
+
+if (a == b) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
